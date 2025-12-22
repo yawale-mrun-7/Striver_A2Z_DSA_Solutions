5ece12a37509b5693124ac86c3b4df96dcb3185c
yawale-mrun-7 <yawale.mrun.7@gmail.com>
2025-12-22 22:27:07 +0530

    Add largest element in array solution

---
+++ 01_Arrays/01_largest_element.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[]={2, 4, 6, 8, 6, 7};
+    int n=sizeof(arr)/sizeof(int);
+    int max=0;
+    for(int i=0; i<n; i++) {
+        if(arr[i]>max) {
+            max=arr[i];
+        }
+    }
+    cout<<"largest element: "<<max<<endl;
+    return 0;
+}
